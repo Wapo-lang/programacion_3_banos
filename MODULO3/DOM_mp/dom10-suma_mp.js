1e47f5abf4ec9a5ae7259338d430089a9fd3e17b
Wapo-lang <cdavid107@hotmail.com>
2026-06-24 09:05:00 -0500

    Clase 22/06/2026

--- MODULO3/DOM_mp/dom10-suma_mp.js
+++ MODULO3/DOM_mp/dom10-suma_mp.js
@@ -1,4 +1,3 @@
-
 const campoNumero1 = document.getElementById('campoNumero1');
 const campoNumero2 = document.getElementById('campoNumero2');
 const btnSumar = document.getElementById('btn_sumar');
@@ -8,5 +7,11 @@ btnSumar.addEventListener('click', function() {
    const numero1 = parseFloat(campoNumero1.value);
    const numero2 = parseFloat(campoNumero2.value);
    const suma = numero1 + numero2;
-   resultado.textContent = `Resultado: ${suma}` ;
-}); 
+   
+   const li = document.createElement('li');
+   li.textContent = `🔮 Suma total de almas: ${isNaN(suma) ? "Inválido" : suma}`;
+   li.style.cssText = "background: #131c2e; border: 1px solid #374151; padding: 10px 14px; margin-top: 10px; border-radius: 6px; max-width: 300px; color: #34d399; font-weight: bold;";
+   
+   resultado.innerHTML = ""; 
+   resultado.appendChild(li);
+});
