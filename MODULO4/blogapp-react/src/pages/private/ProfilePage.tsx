2869184b4bfa8581e36230a715362cd5a8a28534
Mateo2alejo <alejandroviracocha844@gmail.com>
2026-07-14 09:00:00 -0500

    Clase PROGRA

--- MODULO4/blogapp-react/src/pages/private/ProfilePage.tsx
+++ MODULO4/blogapp-react/src/pages/private/ProfilePage.tsx
@@ -1,4 +1,4 @@
 // src/pages/private/ProfilePage.tsx
-export default function ProfilePage() {
-  return <div className="p-8">Perfil — Módulo 08</div>
-}
+import ProfilePage from '@/components/private/ProfilePage'
+
+export default ProfilePage
--- MODULO4/blogapp-react/src/types/user.types.ts
+++ MODULO4/blogapp-react/src/types/user.types.ts
@@ -6,3 +6,8 @@ export interface User {
   isActive: boolean
   profile?: string
 }
+export interface CreateUserPayload {
+  username: string
+  email: string
+  password: string
+}
